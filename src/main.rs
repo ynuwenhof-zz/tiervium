c95b8279f6199fb335fdbb04d86afd0e37cc75d9
Yannic Nuwenhof <ynuwenhof@pm.me>
2022-01-07 19:30:55 +0100

    Add config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use serde::Deserialize;
+
+use tokio::fs;
+
+#[derive(Deserialize)]
+struct Config {
+    mongodb: String,
+    key: String,
+    zones: Vec<String>,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let content = fs::read_to_string("Config.toml").await?;
+    let config: Config = toml::from_str(&content)?;
+
+    Ok(())
 }
