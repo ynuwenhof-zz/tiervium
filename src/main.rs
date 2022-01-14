5ff69fecd95fec8b0442b41e92ab4a50353706a5
Yannic Nuwenhof <ynuwenhof@pm.me>
2022-01-14 12:54:52 +0100

    Add database

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 mod models;
 mod tier;
 
-use tokio::fs;
-use tokio::time;
+use anyhow::{Context, Result};
+use futures::future;
+use models::{Log, Vehicle};
+use mongodb::bson;
+use mongodb::bson::doc;
+use mongodb::options::FindOneOptions;
+use mongodb::options::IndexOptions;
+use mongodb::options::UpdateOptions;
+use mongodb::IndexModel;
+use reqwest::header::HeaderMap;
 use serde::Deserialize;
 use std::sync::Arc;
 use std::time::Duration;
-use anyhow::{Result, Context};
-use reqwest::header::HeaderMap;
-use tier::get_vehicles_by_zone;
+use tokio::fs;
+use tokio::time;
+
+const DATABASE: &str = "tiervium";
+const VEHICLE_COLLECTION: &str = "vehicles";
+const LOG_COLLECTION: &str = "logs";
 
 #[derive(Deserialize)]
 struct Config {
@@ -24,41 +35,125 @@ async fn main() -> Result<()> {
         .await
         .with_context(|| "Failed to read contents from Config.toml")?;
 
-    let config: Config = toml::from_str(&content)
-        .with_context(|| "Failed to parse config")?;
+    let config: Config = toml::from_str(&content).with_context(|| "Failed to parse config")?;
+
+    let mongo = Arc::new(mongodb::Client::with_uri_str(&config.mongodb).await?);
+
+    {
+        let database = mongo.database(DATABASE);
+        let index_options = IndexOptions::builder().unique(true).build();
+
+        let index_model = IndexModel::builder()
+            .keys(doc!("uuid": 1))
+            .options(index_options)
+            .build();
+
+        database
+            .collection::<Vehicle>(VEHICLE_COLLECTION)
+            .create_index(index_model, None)
+            .await?;
+
+        let index_model = IndexModel::builder().keys(doc!("vehicle_uuid": 1)).build();
+
+        database
+            .collection::<Log>(LOG_COLLECTION)
+            .create_index(index_model, None)
+            .await?;
+    }
 
     let mut headers = HeaderMap::new();
     headers.insert("X-Api-Key", config.key.parse()?);
 
-    let http_client = reqwest::Client::builder()
+    let http = reqwest::Client::builder()
         .default_headers(headers)
         .build()?;
 
-    let http_client = Arc::new(http_client);
+    let http = Arc::new(http);
 
     let zones = match config.zones {
         Some(zones) => zones,
-        None => tier::get_zones(&http_client).await
+        None => tier::get_zones(http.clone())
+            .await
             .with_context(|| "Failed to retrieve tier zones")?,
     };
 
     loop {
+        let mut handles = Vec::with_capacity(zones.len());
+
         for zone in &zones {
-            let http_client = http_client.clone();
+            let http = http.clone();
+            let mongo = mongo.clone();
+
             let zone = zone.clone();
 
-            tokio::spawn(async move {
-                handle(http_client, &zone).await
-            });
+            let handle = tokio::spawn(async move { handle(http, mongo, &zone).await });
+
+            handles.push(handle);
         }
 
+        future::join_all(handles).await;
         time::sleep(Duration::from_secs(config.delay)).await;
     }
 }
 
-async fn handle(http_client: Arc<reqwest::Client>, zone: &str) -> Result<()> {
-    let logs = get_vehicles_by_zone(http_client, zone).await?;
-    // TODO: Process the logs
+async fn handle(
+    http: Arc<reqwest::Client>,
+    mongo: Arc<mongodb::Client>,
+    zone: impl AsRef<str>,
+) -> Result<()> {
+    let (vehicles, logs) = tier::get_vehicles_by_zone(http, zone.as_ref()).await?;
+
+    let database = mongo.database(DATABASE);
+
+    let mut session = mongo.start_session(None).await?;
+    session.start_transaction(None).await?;
+
+    for vehicle in &vehicles {
+        database
+            .collection::<Vehicle>(VEHICLE_COLLECTION)
+            .update_one_with_session(
+                doc!("uuid": &vehicle.uuid),
+                doc!("$setOnInsert": bson::to_bson(&vehicle)?.as_document()),
+                UpdateOptions::builder().upsert(true).build(),
+                &mut session,
+            )
+            .await?;
+    }
+
+    session.commit_transaction().await?;
+    session.start_transaction(None).await?;
+
+    let mut new_logs = Vec::new();
+    let log_collection = database.collection::<Log>(LOG_COLLECTION);
+
+    for log in &logs {
+        if let Some(existing_log) = log_collection
+            .find_one_with_session(
+                doc!("vehicle_uuid": &log.vehicle_uuid),
+                FindOneOptions::builder().sort(doc!("time": -1)).build(),
+                &mut session,
+            )
+            .await?
+        {
+            if existing_log.time == log.time
+                || existing_log.lat == log.lat && existing_log.lng == log.lng
+            {
+                continue;
+            }
+        }
+
+        new_logs.push(log);
+    }
+
+    if !new_logs.is_empty() {
+        log_collection
+            .insert_many_with_session(new_logs, None, &mut session)
+            .await?;
+    }
+
+    session.commit_transaction().await?;
+
+    // TODO: Track rented vehicles
 
     Ok(())
 }
--- src/tier.rs
+++ src/tier.rs
@@ -1,12 +1,14 @@
+use crate::models::{Attr, Log, Vehicle};
+use anyhow::{anyhow, Result};
+use chrono::Utc;
+use mongodb::bson;
+use mongodb::bson::Uuid;
+use reqwest::{Client, Url};
+use serde_json::Value;
 use std::sync::Arc;
 
-use serde_json::Value;
-use anyhow::{anyhow, Result};
-
-use crate::models::{VehicleLog, VehicleAttributes};
-
-pub async fn get_zones(http_client: &reqwest::Client) -> Result<Vec<String>> {
-    let content = http_client
+pub async fn get_zones(http: Arc<Client>) -> Result<Vec<String>> {
+    let content = http
         .get("https://platform.tier-services.io/v1/zone?type=root")
         .send()
         .await?
@@ -16,25 +18,30 @@ pub async fn get_zones(http_client: &reqwest::Client) -> Result<Vec<String>> {
     let json: Value = serde_json::from_str(&content)?;
     let data = json["data"]
         .as_array()
-        .ok_or(anyhow!(""))?; // TODO: Proper error response
+        .ok_or(anyhow!("invalid json, expected data array"))?;
 
     let mut zones = Vec::with_capacity(data.len());
 
-    for entry in data {
-        let zone = entry["id"]
+    for item in data {
+        let zone = item["id"]
             .as_str()
-            .ok_or(anyhow!(""))? // TODO: Proper error response
-            .to_string();
+            .ok_or(anyhow!("invalid json, expected zone string"))?;
 
-        zones.push(zone);
+        zones.push(zone.to_string());
     }
 
     Ok(zones)
 }
 
-pub async fn get_vehicles_by_zone(http_client: Arc<reqwest::Client>, zone: &str) -> Result<Vec<VehicleLog>> {
-    let content = http_client
-        .get(format!("https://platform.tier-services.io/v2/vehicle?zoneId={}", zone))
+pub async fn get_vehicles_by_zone(
+    http: Arc<Client>,
+    zone: impl AsRef<str>,
+) -> Result<(Vec<Vehicle>, Vec<Log>)> {
+    let content = http
+        .get(Url::parse_with_params(
+            "https://platform.tier-services.io/v2/vehicle",
+            &[("zoneId", zone)],
+        )?)
         .send()
         .await?
         .text()
@@ -43,38 +50,91 @@ pub async fn get_vehicles_by_zone(http_client: Arc<reqwest::Client>, zone: &str)
     let json: Value = serde_json::from_str(&content)?;
     let data = json["data"]
         .as_array()
-        .ok_or(anyhow!(""))?; // TODO: Proper error response
+        .ok_or(anyhow!("invalid json, expected data array"))?;
 
+    let mut vehicles = Vec::with_capacity(data.len());
     let mut logs = Vec::with_capacity(data.len());
 
-    for entry in data {
-        let id = entry["id"]
+    for item in data {
+        let id = item["id"]
             .as_str()
-            .ok_or(anyhow!(""))? // TODO: Proper error response
-            .to_string();
+            .ok_or(anyhow!("invalid json, expected vehicle id string"))?;
 
-        let attr: VehicleAttributes = serde_json::from_value(entry["attributes"].to_owned())?;
-        let log = VehicleLog::from((id, attr));
+        let attr: Attr = serde_json::from_value(item["attributes"].clone())?;
+
+        let vehicle = Vehicle {
+            uuid: Uuid::parse_str(id)?,
+            code: attr.code,
+            max_speed: attr.max_speed,
+            has_box: attr.has_helmet_box,
+            has_helmet: attr.has_helmet,
+            zone: attr.zone_id,
+            kind: attr.vehicle_type,
+            vendor: attr.iot_vendor,
+            license_plate: attr.licence_plate,
+        };
+
+        vehicles.push(vehicle);
+
+        let log = Log {
+            vehicle_uuid: Uuid::parse_str(id)?,
+            time: bson::DateTime::from(attr.last_location_update.parse::<chrono::DateTime<Utc>>()?),
+            lat: attr.lat,
+            lng: attr.lng,
+            battery: attr.battery_level,
+            rentable: attr.is_rentable,
+            state: attr.state,
+        };
+
         logs.push(log);
     }
 
-    Ok(logs)
+    Ok((vehicles, logs))
 }
 
-pub async fn get_vehicle(http_client: Arc<reqwest::Client>, uuid: &str) -> Result<VehicleLog> {
-    let content = http_client
-        .get(format!("https://platform.tier-services.io/v1/vehicle/{}", uuid))
+#[allow(dead_code)]
+pub async fn get_vehicle_by_uuid(
+    http: Arc<Client>,
+    uuid: impl AsRef<str>,
+) -> Result<(Vehicle, Log)> {
+    let content = http
+        .get(format!(
+            "https://platform.tier-services.io/v1/vehicle/{}",
+            uuid.as_ref()
+        ))
         .send()
         .await?
         .text()
         .await?;
-    
+
     let json: Value = serde_json::from_str(&content)?;
     let id = json["data"]["id"]
         .as_str()
-        .ok_or(anyhow!(""))? // TODO: Proper error response
-        .to_string();
+        .ok_or(anyhow!("invalid json, expected vehicle id string"))?;
 
-    let attr: VehicleAttributes = serde_json::from_value(json["data"]["attributes"].to_owned())?;
-    Ok(VehicleLog::from((id, attr)))
+    let attr: Attr = serde_json::from_value(json["data"]["attributes"].clone())?;
+
+    let vehicle = Vehicle {
+        uuid: Uuid::parse_str(id)?,
+        code: attr.code,
+        max_speed: attr.max_speed,
+        has_box: attr.has_helmet_box,
+        has_helmet: attr.has_helmet,
+        zone: attr.zone_id,
+        kind: attr.vehicle_type,
+        vendor: attr.iot_vendor,
+        license_plate: attr.licence_plate,
+    };
+
+    let log = Log {
+        vehicle_uuid: Uuid::parse_str(id)?,
+        time: bson::DateTime::from(attr.last_location_update.parse::<chrono::DateTime<Utc>>()?),
+        lat: attr.lat,
+        lng: attr.lng,
+        battery: attr.battery_level,
+        rentable: attr.is_rentable,
+        state: attr.state,
+    };
+
+    Ok((vehicle, log))
 }
